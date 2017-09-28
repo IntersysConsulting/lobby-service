6b27e6a644b293d821a2b6320a5e99fdc0e063ca
Edgardo Campos <xotl@Edgardos-MacBook-Pro.local>
2017-09-28 16:48:28 -0500

    Anpother small fix

--- src/utils.js
+++ src/utils.js
@@ -40,7 +40,7 @@ const
                 method: 'POST',
                 url: URL_TO_PRINTING_SERVICE,
                 headers: { 'content-type': 'application/json' },
-                body: { image64 }
+                body: JSON.stringify({ image64 })
             }, (error, res, body) => {
                 if (error) {
                     return reject(error)
